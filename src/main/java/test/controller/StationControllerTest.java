38f745fc6e5814e9ec080ee0fa5b4c0af4607ab7
Ruslan Kuleshov <Russlegreyson@gmail.com>
2017-02-17 18:39:03 +0300

    Almost final changes

---
+++ src/main/java/test/controller/StationControllerTest.java
@@ -0,0 +1,7 @@
+package test.controller;
+
+/**
+ * Created by RuslanKuleshov on 15.02.17.
+ */
+public class StationControllerTest {
+}
---
+++ src/main/java/test/controller/dao/StationDAOTest.java
@@ -0,0 +1,7 @@
+package test.controller.dao;
+
+/**
+ * Created by RuslanKuleshov on 15.02.17.
+ */
+public class StationDAOTest {
+}
